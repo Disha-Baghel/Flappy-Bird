6a15a055e38a6cbcf24ac5fd474cd8190bf9b81d
Disha-Baghel <bagheldisha708@gmail.com>
2023-02-09 17:10:51 +0530

    Added new files

---
+++ game.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+#include<cstdlib>
+#include<SDL2/SDL.h>
+#include"game.hpp"
+
+
---
+++ game.hpp
@@ -0,0 +1,29 @@
+#ifndef game_hpp
+#define game_hpp
+
+#include<string>
+#include<deque>
+#include<SDL2/SDL.h>
+
+class Game{
+	public:
+		Game();
+		void run();
+		~Game();
+	private:
+		SDL_Renderer *renderer;
+		SDL_Window *window;
+		bool running;
+		SDL_Rect bird;
+		std::deque<SDL_Rect> pipes;
+
+		void update();
+		void draw_bird();
+		void draw_pipes();
+		void is_gameOver();
+		void handle_pipes();
+		PipePair generate_pipe_pair();
+		void event_handler();
+		void error_quit(std::string message);
+}
+#endif
